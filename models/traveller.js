e20bd7021b707b0ba3b0bc1837f9a522e103cbb9
Elaina Gillon <elaina.gillon@gmail.com>
2022-06-22 23:29:33 +0100

    tests complete

--- models/traveller.js
+++ models/traveller.js
@@ -16,20 +16,29 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journeys) => {
+    return total += journeys.distance
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+  return this.journeys.map((journey) => {
+    return journey.transport;
+  })
+    .filter((transport, index, array) => array.indexOf(transport) == index);
+}
 
 
 module.exports = Traveller;
